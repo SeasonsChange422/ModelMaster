eb6c692ffadd8f7a83bc5085545478fe4f684e27
lxc <3250162035@qq.com>
2025-09-01 23:19:22 +0800

    init

---
+++ model-master-app/src/main/java/io/github/timemachinelab/modelmasterapp/ModelMasterAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.timemachinelab.modelmasterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelMasterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelMasterAppApplication.class, args);
+    }
+
+}
